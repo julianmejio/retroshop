c91ba1a2791d67ef58335eea2210e1042ffa43db
Julián Mejio <julianmejio@gmail.com>
2026-09-19 16:43:51 +0200

    Add shopping cart page (index) placholder

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,8 +3,13 @@ export default function Home() {
     <div className="h-screen flex items-center justify-center bg-background p-4 lg:p-8">
       <div className="w-full max-w-5xl h-full flex flex-row rounded-2xl overflow-hidden shadow-lg">
         {/* Left panel */}
-        <div className="w-1/2 flex items-center justify-center bg-pink-500 py-16 px-8">
-          <p>Offer here</p>
+        <div className="w-1/2 flex bg-pink-500 py-16 px-8 text-white flex-col">
+          <h1 className="text-3xl font-extrabold">DVD retail store</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            faucibus vestibulum laoreet. Sed a laoreet nibh. Sed mi arcu,
+            porttitor id pellentesque vitae, feugiat ut arcu.
+          </p>
         </div>
 
         {/* Right panel */}
